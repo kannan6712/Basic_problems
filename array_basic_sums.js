6e80dadcae6e05440c7e3c9afbe9884b25ee618b
SURENTHARKUMAR843 <surentharkumarsk843@gmail.com>
2023-09-01 18:22:56 +0530

    add

--- array_basic_sums.js
+++ array_basic_sums.js
@@ -1,4 +1,4 @@
-// 1.
+// problem  1
 let arr=[1,2,3,4,1,2,2,1]
 
 let obj={}
@@ -17,16 +17,16 @@ for(let i of arr){
 console.log(obj);
 
 
-//2.
-let arr = [1, 2, 3, 4, 1, 2, 2, 1];
+//problem 2
+let arr1 = [1, 2, 3, 4, 1, 2, 2, 1];
 
-let obj = {};
+let obj1 = {};
 
-for (let i of arr) {
-  if (i in obj) {
-    obj[i] += 1; //
+for (let i of arr1) {
+  if (i in obj1) {
+    obj1[i] += 1; //
   } else {
-    obj[i] = 1;
+    obj1[i] = 1;
   }
 }
-console.log(obj);
+console.log(obj1);
